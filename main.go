a75c19578d501a4daa206fbada2900a31ed23a0a
Sebastian Kappes <8018-shk@users.noreply.gitlab.lrz.de>
2022-08-17 16:56:17 +0200

    Implement rudimentary message sending

--- main.go
+++ main.go
@@ -2,8 +2,68 @@ package main
 
 import (
 	"fmt"
+	"gitlab.lrz.de/protocol-design-sose-2022-team-0/sanft/messages"
+	"net"
 )
 
 func main() {
-	fmt.Println("Test")
+	/* open a socket */
+	laddr := net.UDPAddr{
+		Port: 1234,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	conn, err := net.ListenUDP("udp", &laddr)
+	if err != nil {
+		fmt.Printf("Error while listening: %v", err)
+		return
+	}
+
+	/* Send an NTM message */
+	Token := [256]uint8{1, 2, 3, 4, 5}
+	sh := messages.ServerHeader{Number: 5}
+	m0 := messages.NTM{Header: sh, Token: Token}
+	err = m0.Send(conn, &laddr)
+	if err != nil {
+		fmt.Printf("Error while sending:  %v", err)
+		return
+	}
+
+	/* Send an MDR message */
+	ch := messages.ClientHeader{Number: 6}
+	m1 := messages.MDR{Header: ch, URI: "/test/bla/blub"}
+	err = m1.Send(conn, &laddr)
+	if err != nil {
+		fmt.Printf("Error while sending:  %v", err)
+		return
+	}
+
+	/* Send an MDRR message */
+	sh = messages.ServerHeader{Number: 7}
+	m2 := messages.MDRR{Header: sh, FileID: 5}
+	err = m2.Send(conn, &laddr)
+	if err != nil {
+		fmt.Printf("Error while sending:  %v", err)
+		return
+	}
+
+	/* Send an ACR message */
+	ch = messages.ClientHeader{Number: 7}
+	cr1 := messages.CR{Length: 5}
+	cr2 := messages.CR{Length: 7}
+	m3 := messages.ACR{Header: ch, CRs: []messages.CR{cr1, cr2}}
+	err = m3.Send(conn, &laddr)
+	if err != nil {
+		fmt.Printf("Error while sending:  %v", err)
+		return
+	}
+
+	/* Send an CRR message */
+	sh = messages.ServerHeader{Number: 7}
+	data := []uint8{1, 2, 3, 4, 5}
+	m4 := messages.CRR{Header: sh, Data: data}
+	err = m4.Send(conn, &laddr)
+	if err != nil {
+		fmt.Printf("Error while sending:  %v", err)
+		return
+	}
 }
---
+++ messages/messages.go
@@ -0,0 +1,60 @@
+package messages
+
+import (
+	"net"
+)
+
+type Message interface {
+	Send(conn *net.UDPConn, addr *net.UDPAddr) error
+}
+
+type ClientHeader struct {
+	Version uint8
+	Type    uint8
+	Number  uint8
+	Token   [256]uint8
+}
+
+type ServerHeader struct {
+	Version uint8
+	Type    uint8
+	Number  uint8
+	Error   uint8
+}
+
+type NTM struct {
+	Header ServerHeader
+	Token  [256]uint8
+}
+
+type MDR struct {
+	Header ClientHeader
+	URI    string /* this must be handled manualy when sending */
+}
+
+type MDRR struct {
+	Header         ServerHeader
+	ChunkSize      uint16
+	MaxChunksInACR uint16
+	FileID         uint32
+	FileSize       [6]uint8 /* alas, there is no uint48 :-( */
+	Checksum       [256]uint8
+}
+
+type CR struct {
+	ChunkOffset [6]uint8 /* note to self: 48-bit field sizes were a dumb idea */
+	Length      uint8
+}
+
+type ACR struct {
+	Header     ClientHeader
+	FileID     uint32
+	PacketRate uint32
+	CRs        []CR
+}
+
+type CRR struct {
+	Header      ServerHeader
+	ChunkNumber [6]uint8 /* :-( */
+	Data        []uint8
+}
