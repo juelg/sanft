438fd0f0f4a426ce7d52206143d80baa2ae9d0d6
Tobias Jülg <tobias@juelg.net>
2022-08-23 13:09:06 +0200

    Fixed upper case error warnings

--- messages/connection.go
+++ messages/connection.go
@@ -19,7 +19,7 @@ func CreateServerSocket(ip string, port int) (*net.UDPConn, error){
 	conn, err := net.ListenUDP("udp", &laddr)
 	// fmt.Println("Hello, World!")
 	if err != nil {
-		return nil, fmt.Errorf("Error creating ListenUDP: %v", err)
+		return nil, fmt.Errorf("error creating ListenUDP: %v", err)
 	}
 	return conn, nil
 }
@@ -27,13 +27,13 @@ func CreateServerSocket(ip string, port int) (*net.UDPConn, error){
 func CreateClientSocket(address string, port int) (*net.UDPConn, error){
 	raddr, err := net.ResolveUDPAddr("udp", address + ":" + fmt.Sprint(port))
 	if err != nil {
-		return nil, fmt.Errorf("Error resolving addr: %v", err)
+		return nil, fmt.Errorf("error resolving addr: %v", err)
 	}
 
 	// take local laddr
 	conn, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
-		return nil, fmt.Errorf("Error dialing to server: %v", err)
+		return nil, fmt.Errorf("error dialing to server: %v", err)
 	}
 	return conn, nil
 }
--- messages/send.go
+++ messages/send.go
@@ -14,11 +14,11 @@ func (m NTM) Send(conn *net.UDPConn, addr *net.UDPAddr) error {
 	// TODO: we want to send big endian, right?
 	err := binary.Write(buf, binary.BigEndian, m)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	_, err = conn.WriteToUDP(buf.Bytes(), addr)
 	if err != nil {
-		return fmt.Errorf("Error sending message: %v", err)
+		return fmt.Errorf("error sending message: %v", err)
 	}
 	return nil
 }
@@ -27,17 +27,17 @@ func (m MDR) Send(conn *net.UDPConn, addr *net.UDPAddr) error {
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.BigEndian, m.Header)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* variable length field of type string must be handled separately */
 	_, err = buf.WriteString(m.URI)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	// _, err = conn.WriteToUDP(buf.Bytes(), addr)
 	_, err = conn.Write(buf.Bytes())
 	if err != nil {
-		return fmt.Errorf("Error sending message: %v", err)
+		return fmt.Errorf("error sending message: %v", err)
 	}
 	return nil
 }
@@ -46,11 +46,11 @@ func (m MDRR) Send(conn *net.UDPConn, addr *net.UDPAddr) error {
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.BigEndian, m)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	_, err = conn.WriteToUDP(buf.Bytes(), addr)
 	if err != nil {
-		return fmt.Errorf("Error sending message: %v", err)
+		return fmt.Errorf("error sending message: %v", err)
 	}
 	return nil
 }
@@ -61,27 +61,27 @@ func (m ACR) Send(conn *net.UDPConn, addr *net.UDPAddr) error {
 	/* encode header */
 	err := binary.Write(buf, binary.BigEndian, m.Header)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* encode FileID */
 	err = binary.Write(buf, binary.BigEndian, m.FileID)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* encode PacketRate */
 	err = binary.Write(buf, binary.BigEndian, m.PacketRate)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* encode CRs */
 	err = binary.Write(buf, binary.BigEndian, m.CRs)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* and finally, send it! */
 	_, err = conn.WriteToUDP(buf.Bytes(), addr)
 	if err != nil {
-		return fmt.Errorf("Error sending message: %v", err)
+		return fmt.Errorf("error sending message: %v", err)
 	}
 	return nil
 }
@@ -91,22 +91,22 @@ func (m CRR) Send(conn *net.UDPConn, addr *net.UDPAddr) error {
 	/* encode header */
 	err := binary.Write(buf, binary.BigEndian, m.Header)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* encode ChunkNumber */
 	err = binary.Write(buf, binary.BigEndian, m.ChunkNumber)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* encode Data */
 	err = binary.Write(buf, binary.BigEndian, m.Data)
 	if err != nil {
-		return fmt.Errorf("Error encoding message: %v", err)
+		return fmt.Errorf("error encoding message: %v", err)
 	}
 	/* …and send it… */
 	_, err = conn.WriteToUDP(buf.Bytes(), addr)
 	if err != nil {
-		return fmt.Errorf("Error sending message: %v", err)
+		return fmt.Errorf("error sending message: %v", err)
 	}
 	return nil
 }
